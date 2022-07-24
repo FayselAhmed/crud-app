95f43b652bb7b87ffd83cdd8532f360c7578895d
Faysel Ahmed <s.m.faysel@gmail.com>
2022-07-24 19:51:09 +0600

    inital commit

---
+++ src/component/pages/Contract.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contract = () => {
+    return (
+        <div className='container'>
+            <h1>Contract Pages</h1>
+        </div>
+    );
+};
+
+export default Contract;
